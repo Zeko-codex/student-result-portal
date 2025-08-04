5016966101485af1493fae42c541612b12e8bd9b
زياد محمد عوض مصطفى عزب <ziad453519@fci.bu.edu.eg>
2025-08-04 19:52:07 +0300

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function login() {
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
- fetch("https://script.google.com/macros/s/AKfycbxrYf2aBcDsFRHsZmnT-O-RosDzoccq66UvYY5Uv5OtUGh0MrL7KWhscJbrbF0jYKM76Q/exec")
+fetch("https://script.google.com/macros/s/AKfycbxcSeSMJNUTJOhgbQhLl4TnLLvkm_VFxwysRtGfHNKrprD5rVx9ErxhAFHQPBA_5Nw9/exec")
     .then(res => res.json())
     .then(data => {
       const user = data.find(d => d.username === username && d.password === password);
