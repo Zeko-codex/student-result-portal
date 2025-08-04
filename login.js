2baf1e88ebb2066daabe3c0f11160f1259468a32
زياد محمد عوض مصطفى عزب <ziad453519@fci.bu.edu.eg>
2025-08-04 20:04:34 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,17 +1,20 @@
-
 function login() {
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-fetch("https://script.google.com/macros/s/AKfycbxcSeSMJNUTJOhgbQhLl4TnLLvkm_VFxwysRtGfHNKrprD5rVx9ErxhAFHQPBA_5Nw9/exec")
+  fetch("https://script.google.com/macros/s/AKfycbxcSeSMJNUTJOhgbQhLl4TnLLvkm_VFxwysRtGfHNKrprD5rVx9ErxhAFHQPBA_5Nw9/exec")
     .then(res => res.json())
     .then(data => {
-      const user = data.find(d => d.username === username && d.password === password);
+      const user = data.find(d => 
+        d.username.toString().trim() === username &&
+        d.password.toString().trim() === password
+      );
+
       if (user) {
         localStorage.setItem("student", JSON.stringify(user));
         window.location.href = "profile.html";
       } else {
-        document.getElementById("error").innerText = "Invalid login credentials.";
+        document.getElementById("error").innerText = "بيانات الدخول غير صحيحة.";
       }
     });
 }
